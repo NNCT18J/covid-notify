c9bc8634293e0b0e74ee5092a10db3f572e8e6a9
BonyChops <office.kentarou@gmail.com>
2021-01-14 02:02:19 +0900

    Init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # corona-notify
 Send notify about corona info to Discord
+
+# Data form
+[新型コロナウイルス(COVID-19)感染症の対応について｜内閣官房新型コロナウイルス感染症対策推進室](https://corona.go.jp/dashboard/)
---
+++ app.js
@@ -0,0 +1,38 @@
+const cron = require('node-cron');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync("./config.json"));
+let savedData = fs.existsSync('./data.json') ? JSON.parse(fs.readFileSync("./data.json")) : { num: 0 };
+if (!fs.existsSync('./data.json')) {
+    fs.writeFileSync('./data.json', JSON.stringify({ num: 0 }));
+}
+
+const job = () => {
+    fetch(config.dataURI).then(async (data) => {
+        const cache = await data.json();
+        const num = cache[0].area.find(el => el.name_jp == config.pref).npatients;
+        console.log(num);
+        if (Number(savedData.num) != Number(num)) {
+            const diff = Number(num) - Number(savedData.num);
+            fetch(config.postTo, {
+                "headers": {
+                    "content-type": "application/json",
+                },
+                "body": JSON.stringify({
+                    "username": "Nagano_Corona",
+                    "avatar_url": "https://pbs.twimg.com/profile_images/1247716266388774912/yOr0f8yU_400x400.jpg",
+                    "content": `${new Date().toISOString()}に更新されました\n${config.pref}:\`\`\`diff\n${savedData.num}人 → ${num}人\n${diff > 0 ? "+" : ""}${diff}人\n\`\`\``
+                }),
+                "method": "POST",
+            });
+            savedData.num = num;
+            fs.writeFileSync('./data.json', JSON.stringify(savedData));
+        }
+    });
+}
+
+job();
+
+cron.schedule("*/5 * * * *", () => {
+    job();
+})
